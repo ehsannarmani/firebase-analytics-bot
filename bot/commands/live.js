d5c9041595a0ffa3191d77f9f94f3d01df41acb2
Ehsan Narmani <ehsan.enk.narmani@gmail.com>
2025-01-21 13:53:48 +0330

    improve live update logic

--- bot/commands/live.js
+++ bot/commands/live.js
@@ -1,29 +1,33 @@
 const { getActiveUsersLast30Minutes } = require('../services/analytics');
 const { getFormattedDate } = require('../services/dateUtils');
 
-const liveUpdates = new Set();
-
+const subscribedMessages = new Set();
+let updateInterval = null
 function setupLiveCommand(bot) {
     bot.command("live", async (ctx) => {
+        if(subscribedMessages.size === 0){
+            startUpdate(bot,5)
+        }
         const message = await ctx.reply("Starting live update for last 30 minutes active users...");
-        const update = async () => {
-            try {
-                const report = await getActiveUsersLast30Minutes();
-                await bot.api.editMessageText(
-                    message.chat.id,
-                    message.message_id,
-                    `🛜 Live Update\n\n📍 Active users in last 30 minutes: <code>${report}</code>\n\nLast Update: ${getFormattedDate()}`,
-                    { parse_mode: 'HTML' }
-                );
-            } catch (e) { }
-        };
-        await update();
-        const intervalId = setInterval(update, 5000);
-        liveUpdates.add({
-            messageId: message.message_id,
-            intervalId: intervalId
-        });
+        subscribedMessages.add({messageId: message.message_id, chatId: message.chat.id})
     });
 }
 
-module.exports = { setupLiveCommand, liveUpdates };
+function startUpdate(bot,duration){
+    updateInterval = setInterval(async function (){
+        const result = await getActiveUsersLast30Minutes()
+        for (const message of subscribedMessages) {
+            await bot.api.editMessageText(
+                message.chatId,
+                message.messageId,
+                `🛜 Live Update\n\n📍 Active users in last 30 minutes: <code>${result}</code>\n\nLast Update: ${getFormattedDate()}`,
+                { parse_mode: 'HTML' }
+            );
+        }
+    },duration*1000)
+}
+function stopLive(){
+    clearInterval(updateInterval)
+}
+
+module.exports = { setupLiveCommand, stopLive, subscribedMessages };
--- bot/commands/stop.js
+++ bot/commands/stop.js
@@ -1,20 +1,20 @@
-const { liveUpdates } = require('./live');
+const {subscribedMessages, stopLive} = require("./live");
 
 function setupStopCommand(bot) {
     bot.command("stop", async (ctx) => {
         try {
             if (ctx.message.reply_to_message) {
                 const repliedMessageId = ctx.message.reply_to_message.message_id;
-                const foundLiveUpdate = Array.from(liveUpdates).filter((value) => value.messageId == repliedMessageId)[0];
-                if (foundLiveUpdate) {
-                    clearInterval(foundLiveUpdate.intervalId);
+                const subscribedMessage = Array.from(subscribedMessages).filter((value) => value.messageId == repliedMessageId)[0];
+                if (subscribedMessage) {
+                    subscribedMessages.delete(subscribedMessage);
                     let liveUpdateText = ctx.message.reply_to_message.text;
 
                     liveUpdateText = liveUpdateText.split("Live Update");
                     const newText = `${liveUpdateText[0]} Live Update - Stopped ❌${liveUpdateText[1]}`;
                     await bot.api.editMessageText(ctx.chat.id, repliedMessageId, newText, { parse_mode: 'HTML' });
                     await ctx.reply("Replied live update stopped. ✅");
-                    liveUpdates.delete(foundLiveUpdate);
+                    if(subscribedMessages.size == 0) stopLive()
                 } else {
                     await ctx.reply("❌ Live update is not found, maybe it's stopped or something.");
                 }
