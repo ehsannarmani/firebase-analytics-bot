f29e76d6a77f893a9f351b02ca4d197b8b11052d
Ehsan Narmani <ehsan.enk.narmani@gmail.com>
2025-01-21 13:57:55 +0330

    update commands

--- bot/commands/configure.js
+++ bot/commands/configure.js
@@ -1,10 +1,12 @@
 const commands = [
-    {command: 'start', description: 'Start the bot'},
-    {command: 'daily', description: 'Get daily active users report'},
-    {command: 'min30', description: 'Get last 30 minutes active users'},
-    {command: 'users', description: 'Get total lifetime users'},
-    {command: 'countries', description: 'Get total lifetime users by countries'},
-]
+    { command: 'start', description: 'Start the bot and view available commands.' },
+    { command: 'daily', description: 'Get a report of active users over the last 7 days.' },
+    { command: 'min30', description: 'Get the number of active users in the last 30 minutes.' },
+    { command: 'users', description: 'Get the total number of lifetime users.' },
+    { command: 'countries', description: 'Get the total lifetime users grouped by country.' },
+    { command: 'live', description: 'Start live updates for active users in the last 30 minutes (updates every 5 seconds).' },
+    { command: 'stop', description: 'Stop the live update by replying to the live update message.' },
+];
 
 function configureCommands(bot) {
     bot.api.setMyCommands(commands)
